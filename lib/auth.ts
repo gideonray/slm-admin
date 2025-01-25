cdf597d281b662c7c9e1228e1a30490e394a2ce6
Gideon Caranzo <gideonray@gmail.com>
2025-01-24 20:31:10 -0600

    updated Google provider params

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,9 +3,6 @@ import Google from 'next-auth/providers/google';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET
-    })
+    Google
   ]
 });
